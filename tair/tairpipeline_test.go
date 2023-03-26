d8cfd1ca8b3a52415f400a70ffe0f8aeec52e800
xuan <admin@exuan.org>
2023-03-26 15:28:47 +0800

    feat: tairpipeline test

--- tair/tairpipeline_test.go
+++ tair/tairpipeline_test.go
@@ -1,20 +1,33 @@
-package tair
+package tair_test
 
 import (
 	"context"
 	"testing"
 
+	"github.com/alibaba/tair-go/tair"
 	"github.com/go-redis/redis/v8"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestTairPipeline(t *testing.T) {
-	opt := &redis.Options{
-		Addr: "localhost:6379",
-	}
-	client := NewTairClient(opt)
-	ctx := context.Background()
-	pipe := client.TairPipeline()
+type PipelineTestSuite struct {
+	suite.Suite
+	tairClient        *tair.TairClient
+	tairClusterClient *tair.TairClusterClient
+}
+
+func (suite *PipelineTestSuite) SetupTest() {
+	suite.tairClient = tair.NewTairClient(redisOptions())
+	assert.Equal(suite.T(), "OK", suite.tairClient.FlushDB(ctx).Val())
+	suite.tairClusterClient = tair.NewTairClusterClient(&tair.TairClusterOptions{ClusterOptions: redisClusterOptions()})
+	err := suite.tairClusterClient.ForEachMaster(ctx, func(ctx context.Context, master *redis.Client) error {
+		return master.FlushDB(ctx).Err()
+	})
+	assert.NoError(suite.T(), err)
+}
+
+func (suite *PipelineTestSuite) TestTairPipeline(t *testing.T) {
+	pipe := suite.tairClient.TairPipeline()
 	pipe.Set(ctx, "key", "value", 0)
 	pipe.Get(ctx, "key")
 	cmds, err := pipe.Exec(ctx)
@@ -22,18 +35,35 @@ func TestTairPipeline(t *testing.T) {
 	assert.Equal(t, "value", cmds[1].(*redis.StringCmd).Val())
 }
 
-func TestTairPipelined(t *testing.T) {
-	opt := &redis.Options{
-		Addr: "localhost:6379",
-	}
-	client := NewTairClient(opt)
-	ctx := context.Background()
-
-	cmds, err := client.TairPipelined(ctx, func(p redis.Pipeliner) error {
+func (suite *PipelineTestSuite) TestTairPipelined(t *testing.T) {
+	cmds, err := suite.tairClient.TairPipelined(ctx, func(p redis.Pipeliner) error {
 		p.Set(ctx, "key", "value", 0)
 		p.Get(ctx, "key")
 		return nil
 	})
 	assert.NoError(t, err)
 	assert.Equal(t, "value", cmds[1].(*redis.StringCmd).Val())
+}
+
+func (suite *PipelineTestSuite) TestTairClusterPipeline(t *testing.T) {
+	pipe := suite.tairClusterClient.TairPipeline()
+	pipe.Set(ctx, "key", "value", 0)
+	pipe.Get(ctx, "key")
+	cmds, err := pipe.Exec(ctx)
+	assert.NoError(t, err)
+	assert.Equal(t, "value", cmds[1].(*redis.StringCmd).Val())
+}
+
+func (suite *PipelineTestSuite) TestTairClusterPipelined(t *testing.T) {
+	cmds, err := suite.tairClusterClient.TairPipelined(ctx, func(p redis.Pipeliner) error {
+		p.Set(ctx, "key", "value", 0)
+		p.Get(ctx, "key")
+		return nil
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, "value", cmds[1].(*redis.StringCmd).Val())
+}
+
+func TestTairPipelineTestSuite(t *testing.T) {
+	suite.Run(t, new(PipelineTestSuite))
 }
